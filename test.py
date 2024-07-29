11b4ae223a772c26104629ec9f933762a1ec4308
Brianna Tieu <briannatieu100@gmail.com>
2024-07-29 14:53:11 -0400

    new stuff

--- test.py
+++ test.py
@@ -1,19 +1,53 @@
+#testing natural language processing
 import nltk
-from nltk.tokenize import word_tokenize
+from nltk import word_tokenize, WordNetLemmatizer, pos_tag, ne_chunk
 from nltk.corpus import stopwords
-from nltk.stem import WordNetLemmatizer
 
 nltk.download('punkt')
 nltk.download('stopwords')
 nltk.download('wordnet')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('maxent_ne_chunker')
+nltk.download('words')
+
 
 lemmatizer = WordNetLemmatizer()
 stop_words = set(stopwords.words('english'))
 
+
+#process the text to get the basic gist / important words
 def preprocess(text):
     words = word_tokenize(text)
     words = [lemmatizer.lemmatize(word.lower()) for word in words if word.isalpha() and word not in stop_words]
     return words
 
-text = "Set a reminder to call John at 5 PM"
+def find_locations(text):
+    # Tokenize the text into sentences
+    sens = nltk.sent_tokenize(text)
+    locations = []
+
+    for sen in sens:
+        # Tokenize each sentence into words
+        words = word_tokenize(sen)
+        # POS tagging
+        tags = pos_tag(words)
+        # Named Entity Recognition
+        location = ne_chunk(tags, binary=False)
+
+        for chunk in location:
+            if hasattr(chunk, 'label') and chunk.label() == 'GPE':
+                    city = ' '.join(c[0] for c in chunk)
+                    locations.append(city)
+
+    return locations
+
+text = "weather at 5 in New York and London"
+important_words = preprocess(text)
 print(preprocess(text))
+
+if 'weather' in important_words:
+    location = find_locations(text)
+    if location:
+        print('Weather location:', location)
+    else:
+        print('Please provide a city name for the weather')
