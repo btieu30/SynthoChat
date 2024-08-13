4a2984058d5228ad87b59b319a985a9e8e2e91dd
Brianna Tieu <briannatieu100@gmail.com>
2024-08-13 15:32:03 -0400

    updating flask

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def display():
-    return render_template('html/home.html')
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.debug = True
