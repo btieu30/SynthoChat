d444cfd255d96dad59af32e963e8cc5745e19fc8
Brianna Tieu <btieu30@stuy.edu>
2024-08-22 14:10:52 -0400

    name changes, comments

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+#home route, displays the home.html template
 @app.route('/')
 def display():
     return render_template('home.html')
